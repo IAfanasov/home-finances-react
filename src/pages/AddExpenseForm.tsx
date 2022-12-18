38363227759061a0ce3b715d7a2cebcc0d11018b
igor.afanasov <igor.afanasov@cloudkitchens.com>
2022-12-18 08:44:16 +0100

    add expence form UX improvements

--- src/pages/AddExpenseForm.tsx
+++ src/pages/AddExpenseForm.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useContext, useState } from "react";
+import { FormEvent, useCallback, useContext, useState } from "react";
 import './AddExpenseForm.css';
 import { HomeFinanceDataContext } from "../shared/data-context";
 import { appendExpences } from '../google-sheets/appendExpences';
@@ -14,16 +14,18 @@ enum ExpenseFormField {
 }
 
 export function AddExpensePage() {
-    const initialState: { wasValidate: boolean } = { wasValidate: false };
-    const [state, setState] = useState(initialState);
+    const [wasValidate, setWasValidated] = useState<boolean>();
+    const [submitting, setSubmitting] = useState<boolean>(false);
     const { data: homeFinanceData } = useContext(HomeFinanceDataContext);
+    const today = new Date();
+    const todayStr = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`
 
-    async function onSubmit(event: FormEvent) {
-        setState(current => ({ ...current, wasValidate: true }));
+    const onSubmit = useCallback(async function onSubmit(event: FormEvent) {
         event.preventDefault();
         const formElem = event.target as HTMLFormElement;
         const isValid = formElem.checkValidity();
         if (!isValid) {
+            setWasValidated(true);
             return;
         }
         const formData = new FormData(formElem);
@@ -39,73 +41,88 @@ export function AddExpensePage() {
             ].map(x => formData.get(x) as string)
         ];
 
-        await appendExpences(values);
-    }
+        setSubmitting(true);
+        try {
+            await appendExpences(values);
+            setWasValidated(false);
+            (event.target as HTMLFormElement).reset();
+        } catch (error) {
+            console.error(error);
+            alert(JSON.stringify(error));
+        } finally {
+            setSubmitting(false);
+        }
+    }, [setWasValidated, setSubmitting])
 
 
-    return <form className={(state.wasValidate ? 'was-validated' : 'needs-validation') + ' col-xxl-3 col-xl-4 col-lg-5 col-md-6 col-sm-7 col-12'}
-            onSubmit={onSubmit}
-            noValidate>
+    return <form className={(wasValidate ? 'was-validated' : 'needs-validation') + ' col-xxl-3 col-xl-4 col-lg-5 col-md-6 col-sm-7 col-12'}
+        onSubmit={onSubmit}
+        noValidate>
 
-            <h3>Add an expense</h3>
+        <h3>Add an expense</h3>
 
-            <div className='input-group mb-3'>
-                <input type="date"
-                    name={ExpenseFormField.date}
-                    required
-                    className="form-control"
-                    placeholder="Date" />
-            </div>
-
-            <div className='input-group mb-3'>
-                <input type="number"
-                    min={0}
-                    step={0.01}
-                    className="form-control"
-                    required
-                    name={ExpenseFormField.amount}
-                    placeholder="Amount" />
-                <select className="currency-select form-select flex-grow-0 flex-shrink-0"
-                    name={ExpenseFormField.currency}>
-                    {homeFinanceData!.currencies.map(currency =>
-                        <option key={currency}>{currency}</option>
-                    )}
-                </select>
-            </div>
-
-            <select defaultValue=''
+        <div className='input-group mb-3'>
+            <input type="date"
+                name={ExpenseFormField.date}
                 required
-                name={ExpenseFormField.category}
-                className="form-select mb-3">
-                <option value="">Select a category</option>
-                {homeFinanceData!.expenseCategories.map(category =>
+                className="form-control"
+                placeholder="Date"
+                defaultValue={todayStr} />
+        </div>
+
+        <div className='input-group mb-3'>
+            <input type="number"
+                min={0}
+                step={0.01}
+                className="form-control"
+                required
+                name={ExpenseFormField.amount}
+                placeholder="Amount" />
+            <select className="currency-select form-select flex-grow-0 flex-shrink-0"
+                name={ExpenseFormField.currency}>
+                {homeFinanceData!.currencies.map(currency =>
+                    <option key={currency}>{currency}</option>
+                )}
+            </select>
+        </div>
+
+        <select defaultValue=''
+            required
+            name={ExpenseFormField.category}
+            className="form-select mb-3">
+            <option value="">Select a category</option>
+            {homeFinanceData!.expenseCategories
+                .sort((a, b) => a.name > b.name ? 1 : -1)
+                .map(category =>
                     <option key={category.name}>{category.name}</option>
                 )}
-            </select>
+        </select>
 
-            <select defaultValue=''
-                required
-                name={ExpenseFormField.account}
-                className="form-select mb-3">
-                <option value="">Select an account</option>
-                {homeFinanceData!.accounts.map(account =>
-                    <option key={account}>{account}</option>
-                )}
-            </select>
+        <select defaultValue='Cash'
+            required
+            name={ExpenseFormField.account}
+            className="form-select mb-3">
+            <option value="">Select an account</option>
+            {homeFinanceData!.accounts.map(account =>
+                <option key={account}>{account}</option>
+            )}
+        </select>
 
-            <div className="mb-3">
-                <label htmlFor="inputComment"
-                    className="form-label">Comment</label>
-                <textarea className="form-control"
-                    id="inputComment"
-                    name={ExpenseFormField.comment}
-                    rows={2}></textarea>
-            </div>
+        <div className="mb-3">
+            <label htmlFor="inputComment"
+                className="form-label">Comment</label>
+            <textarea className="form-control"
+                id="inputComment"
+                name={ExpenseFormField.comment}
+                rows={2}></textarea>
+        </div>
 
-            <button type='submit'
-                className="btn btn-dark ms-auto d-block">Save
-            </button>
-        </form>
+        <button type='submit'
+            disabled={submitting}
+            className="btn btn-dark ms-auto d-block">
+            Save
+        </button>
+    </form>
 }
 
 export default AddExpensePage;
